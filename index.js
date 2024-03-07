16eb8d9cdbf86c449e3e428b8cd2a3410f5b2507
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-03-07 16:30:11 -0700

    added shape js.test code and shape.js code

---
+++ index.js
@@ -0,0 +1,15 @@
+// require shape.js
+const shape = require('shape.js')
+// create a prompt so the user can enter up to 3 characters ofr the logo
+
+
+// Create prompt for the text color where you can enter a color or hexadecimal number
+
+
+// Present a list of shapes from circle, triangle, square
+
+
+
+// genereate logo.svg
+
+
