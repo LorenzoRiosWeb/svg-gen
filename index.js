7dda9a297602942ae38e4178117a30bfbb866878
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-03-07 18:19:31 -0700

    added code to index.js

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 // require shape.js
 const {Shape,Circle,Square,Triangle} = require('shape');
-const inquirer = require('inquirer');
+const Inquirer = require('inquirer');
 const fs = require('fs')
 // create a prompt so the user can enter up to 3 characters for the logo
 
+function validate(input){
+    return input.length ===3;
+}
+const Questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo',
+        validate: (input) => input.length <= 3,
+        
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color (keyword or hex):',
+        
+    },
+    {
+        type: 'input',
+        name: 'shape',
+        message: 'Choose a shape',
+        choice: ['Triangle', 'Circle', 'Square'],
+        
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (keyword or hex):',
+        
+    },
+];
+// Create prompt that call questions array
+Inquirer.prompt(Questions).then((answers) => {
+    const {text , textColor, shape, shapeColor} = answers;
+    const shapeObj = new Shape();
+    let svgElement = '';
 
-// Create prompt for the text color where you can enter a color or hexadecimal number
+    shapeObj.setColor(shapeColor);
+    switch (shape) {
+        case 'Circle':
+            const circle = new Circle();
+            circle.setColor(shapeColor);
+            svgElement = circle.render() 
+            break;
+    
+        default:
+            break;
+    }
+})
 
 
-// Present a list of shapes from circle, triangle, square
 
 
 
-// genereate logo.svg
+
+// generate logo.svg
 
 
